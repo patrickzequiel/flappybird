23814737369501a98db1e10ecfc66b558baa6af6
Patrick Cruz <patrickcruz@MacBook-Pro-de-Patrick.local>
2020-07-31 23:10:12 -0300

    tela azul

--- jogo.js
+++ jogo.js
@@ -87,14 +87,77 @@ const flappyBird = {
     }
 }
 
+const mensagemGetReady = {
+    sX: 134,
+    sY: 0,
+    w: 174,
+    h: 152,
+    x: (canvas.width / 2) - 174 / 2,
+    y: 50,
+    desenha() {
+        contexto.drawImage(
+            sprites,
+            mensagemGetReady.sX, mensagemGetReady.sY,
+            mensagemGetReady.w, mensagemGetReady.h,
+            mensagemGetReady.x, mensagemGetReady.y,
+            mensagemGetReady.w, mensagemGetReady.h,
+        );
+    }
+}
+
+//
+// [Telas]
+//
+
+let telaAtiva = {}
+
+function mudaParaTela(novaTela){
+    telaAtiva = novaTela;
+}
+
+const Telas = { 
+    INICIO: {
+        desenha(){
+            chao.desenha();
+            planoDeFundo.desenha();
+            flappyBird.desenha();
+            mensagemGetReady.desenha();
+        },
+        click(){
+            mudaParaTela(Telas.JOGO);
+        },
+        atualiza() {
+
+        }
+    }
+};
+
+Telas.JOGO = {
+    desenha() {
+        planoDeFundo.desenha();
+        chao.desenha();
+        flappyBird.desenha();
+    },
+   atualiza(){
+       flappyBird.atualiza()
+   }
+}
 
 function loop() {
-    planoDeFundo.desenha();
-    flappyBird.desenha();
-    chao.desenha();
-    flappyBird.atualiza();
+    telaAtiva.desenha();
+    telaAtiva.atualza();
+
 
     requestAnimationFrame(loop);
 }
 
+window.addEventListener('click', function() {
+    if(telaAtiva.click) {
+        telaAtiva.click();
+    }
+});
+
+mudaParaTela(Telas.JOGO);
 loop();
+
+
