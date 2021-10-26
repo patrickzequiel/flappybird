15f7c3ec450befc788b6daaa2ae11491b2c7e4ab
Patrick Cruz <patrickezequielscruz@gmail.com>
2021-10-26 16:31:12 +0200

    Finished

--- jogo.js
+++ jogo.js
@@ -1,6 +1,3 @@
-console.log('[DevSoutinho] Flappy Bird');
-console.log('Inscreva-se no canal :D https://www.youtube.com/channel/UCzR2u5RWXWjUh7CwLSvbitA');
-
 let frames = 0;
 const som_HIT = new Audio();
 som_HIT.src = './efeitos/hit.wav';
